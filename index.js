5aa083d44baac4273a3e8d44aa2f9b9c29294b7b
Leref <juansantana75@icloud.com>
2021-01-07 14:02:34 -0500

    Released

---
+++ index.js
@@ -0,0 +1,49 @@
+const dbd = require("dbd.js")
+const config = require("./config.json")
+const bot = new dbd.Bot({
+sharding: false,
+shardAmount: 2,
+token: config.token,
+prefix: ['$getServerVar[prefix]', '!']
+})
+ 
+bot.onMessage({
+    guildOnly: true
+  })
+
+
+//Music Event
+bot.onMusicStart()
+bot.musicStartCommand({
+ channel: "$channelID", 
+ code: `
+ $color[GREEN]
+ $author[Now Playing]
+ $description[Playing $songInfo[title]]`
+})
+
+
+const fs = require('fs')
+
+const folders = fs.readdirSync("./commands/")
+
+for (const files of folders) {
+const folder = fs.readdirSync(`./commands/${files}/`).filter(file => file.endsWith(".js"))
+
+for (const commands of folder) {
+const command = require(`./commands/${files}/${commands}`) 
+bot.command({
+name: command.name,
+aliases: command.aliases,
+code: command.code
+}) // Variables
+bot.variables({
+    prefix: "!"
+}) // Status
+bot.status({
+  text: "to $serverCount servers 🎶",
+  type: "LISTENING",
+  time: 12
+})
+} 
+}
